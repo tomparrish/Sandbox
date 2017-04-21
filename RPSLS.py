b9c60468d1fc1de56c729254683c03d220004234
Tom Parrish <tomparrish@yahoo.com>
2017-04-21 12:55:21 -0700

    Update RPSLS.py
    got rid of camelCase use in strings

--- RPSLS.py
+++ RPSLS.py
@@ -2,77 +2,77 @@ import time
 from time import sleep
 import random
 
-endOfGame="_"*40
+end_of_game="_"*40
 wins=0
 losses=0
 draws=0
-legalAction=["rock","paper","scissors", "lizard", "Spock"]
+legal_action=["rock","paper","scissors", "lizard", "Spock"]
 while True:
-    userAction=input("rock, paper, scissors, lizard, Spock: ")
-    if userAction not in legalAction:
+    user_action=input("rock, paper, scissors, lizard, Spock: ")
+    if user_action not in legal_action:
         print ("That is not a valid choice")
         continue
         
-    computerAction=random.choice(legalAction)
+    computer_action=random.choice(legal_action)
     sleep(0.5)
-    print (('Computer picked {}.').format(computerAction))
-    if userAction == computerAction:
+    print (('Computer picked {}.').format(computer_action))
+    if user_action == computer_action:
         sleep(0.5)
         draws += 1
-        print (("\nIt's a draw.\n{}").format(endOfGame))
+        print (("\nIt's a draw.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "rock" and computerAction == "scissors":
+    elif user_action == "rock" and computer_action == "scissors":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Rock crushes scissors.\n{}").format(endOfGame))
+        print (("\nYou win.  Rock crushes scissors.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "paper" and computerAction == "rock":
+    elif user_action == "paper" and computer_action == "rock":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Paper covers rock.\n{}").format(endOfGame))
+        print (("\nYou win.  Paper covers rock.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "scissors" and computerAction == "paper":
+    elif user_action == "scissors" and computer_action == "paper":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Scissors cuts paper.\n{}").format(endOfGame))
+        print (("\nYou win.  Scissors cuts paper.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "rock" and computerAction == "lizard":
+    elif user_action == "rock" and computer_action == "lizard":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Rock crushes lizard.\n{}").format(endOfGame))
+        print (("\nYou win.  Rock crushes lizard.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "lizard" and computerAction == "Spock":
+    elif user_action == "lizard" and computer_action == "Spock":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Lizard poisons Spock.\n{}").format(endOfGame))
+        print (("\nYou win.  Lizard poisons Spock.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "Spock" and computerAction == "scissors":
+    elif user_action == "Spock" and computer_action == "scissors":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Spock smashes scissors.\n{}").format(endOfGame))
+        print (("\nYou win.  Spock smashes scissors.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "scissors" and computerAction == "lizard":
+    elif user_action == "scissors" and computer_action == "lizard":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Scissors decapitate Lizard.\n{}").format(endOfGame))
+        print (("\nYou win.  Scissors decapitate Lizard.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "lizard" and computerAction == "paper":
+    elif user_action == "lizard" and computer_action == "paper":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Lizard eats paper.\n{}").format(endOfGame))
+        print (("\nYou win.  Lizard eats paper.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "paper" and computerAction == "Spock":
+    elif user_action == "paper" and computer_action == "Spock":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Paper disproves Spock.\n{}").format(endOfGame))
+        print (("\nYou win.  Paper disproves Spock.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
-    elif userAction == "Spock" and computerAction == "rock":
+    elif user_action == "Spock" and computer_action == "rock":
         sleep(0.5)
         wins += 1
-        print (("\nYou win.  Spock vaporizes Rock.\n{}").format(endOfGame))
+        print (("\nYou win.  Spock vaporizes Rock.\n{}").format(end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
     else:
         sleep(0.5)
         losses += 1
-        print (("\nYou lose. {} loses to {}\n").format(userAction, computerAction, endOfGame))
+        print (("\nYou lose. {} loses to {}\n").format(user_action, computer_action, end_of_game))
         print ("Your record is " + str(wins) + "-" + str(losses) + "-" + str(draws))
