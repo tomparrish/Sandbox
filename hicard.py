aeb4eece5c06c107ea65e8f971b1cb1692b4730c
Tom Parrish <tomparrish@yahoo.com>
2017-04-21 11:14:49 -0700

    Update hicard.py
    Added commentary function and set a handful of fun comments.

--- hicard.py
+++ hicard.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 suits = ["spades", "hearts", "clubs", "diamonds"]
 ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
@@ -6,11 +7,26 @@ ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
 def randomcard():
     card = random.choice(ranks) + " of " + random.choice(suits)
     return card
+    
+def commentary(playercard):
+    comment = ""
+    if "Ace of spades" in playercard:
+        comment = "The Ace of spades!"
+    elif "Ace" in playercard:
+        comment = "He's got the Ace!"
+    elif "King of hearts" in playercard or "King of diamonds" in playercard:
+        comment = "Don't do it!  Life is worth living!  (Suicide King)"
+    elif "King" in playercard:
+        comment = "Cowboy Up!"
+    return comment
 
 def main():
     playercard = randomcard()
     computercard = randomcard()
     print ("Your card is " + playercard)
     print ("My card is " + computercard)
+    sleep(0.5)
+    comment = commentary(playercard)
+    print (comment)
     
 main()
