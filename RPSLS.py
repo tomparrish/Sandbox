85da41d0cee12d1142aed30d50826d12b4df84f7
Tom Parrish <tomparrish@yahoo.com>
2023-05-17 17:46:26 -0700

    Update RPSLS.py
    Adds a variety of actions.
    formally calls main()

--- RPSLS.py
+++ RPSLS.py
@@ -14,29 +14,30 @@ legal_action=["rock","paper","scissors", "lizard", "spock"]
 
 # array to hold the rules of the game
 rules = [
-    ["scissors", "cuts", "paper"],
-    ["paper", "covers", "rock"],
-    ["rock", "crushes", "lizard"],
-    ["lizard", "poisons", "spock"],
-    ["spock", "smashes", "scissors"],
-    ["scissors", "decapitates", "lizard"],
-    ["lizard", "eats", "paper"],
-    ["paper", "disproves", "spock"],
-    ["spock", "vaporizes", "rock"],
-    ["rock", "crushes", "scissors"]]
+    ["scissors", ["cuts", "snips", "notches"], "paper"],
+    ["paper", ["covers", "wraps", "veils"], "rock"],
+    ["rock", ["crushes" "chokes", "kablooeys"], "lizard"],
+    ["lizard", ["poisons", "bites", "claws"], "spock"],
+    ["spock", ["smashes", "destroys", "wallops"], "scissors"],
+    ["scissors", ["decapitates", "stabs", "cuts the head off of"], "lizard"],
+    ["lizard", ["eats", "mutates and spits acid on", "claws"], "paper"],
+    ["paper", ["disproves", "gives a nasty papercut to", "analyzes"], "spock"],
+    ["spock", ["vaporizes", "decimates", "pulverizes"], "rock"],
+    ["rock", ["crushes", "bend", "ruin"], "scissors"]]
 
 # check the rules and determine who wins the round
 def determineWinner(ua, ca):
     global wins, losses, draws
+    salt = random.randint(1,3)
     if ua == ca:
         print ("\nIt's a draw.\n")
         draws += 1
     for i in rules:
         if i[0] == ua and i[2] == ca:
-            print("\nYou win!", i[0], i[1], i[2],"\n")
+            print("\nYou win!", i[0], i[1][salt], i[2],"\n")
             wins += 1
         elif i[0] == ca and i[2] == ua:
-            print("You lose!", i[0], i[1], i[2], "\n") 
+            print("You lose!", i[0], i[1][salt], i[2], "\n") 
             losses += 1
     
 # print current score
@@ -51,17 +52,20 @@ def thinking():
             print(f"\r{cursor}", end="", flush=True)
 
 # main logic (should we add an option to quit?)
-while True:
-    # ask user for input (how about we make this case insensitive or offer shortcuts?)
-    user_action=input("Select your action (rock, paper, scissors, lizard, spock): ")
-    if user_action not in legal_action:
-        print ("That is not a valid choice")
-        continue
+def main():
+    while True:
+        # ask user for input (how about we make this case insensitive or offer shortcuts?)
+        user_action=input("Select your action (rock, paper, scissors, lizard, spock): ")
+        if user_action not in legal_action:
+            print ("That is not a valid choice")
+            continue
         
-    # computer selects action
-    computer_action=random.choice(legal_action)
-    thinking()
-    print (('\nComputer picked {}.\n').format(computer_action))
+        # computer selects action
+        computer_action=random.choice(legal_action)
+        thinking()
+        print (('\nComputer picked {}.\n').format(computer_action))
 
-    determineWinner(user_action, computer_action)
-    printScore()
+        determineWinner(user_action, computer_action)
+        printScore()
+    
+main()
