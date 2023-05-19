37ca4ab2e2a531ecd54fa76c0e871cff6901b2dc
kasclark <kasclark@runbox.com>
2023-05-19 15:23:45 -0700

    added visualization

--- hangman.py
+++ hangman.py
@@ -22,6 +22,7 @@ def play(word):
     turns = 10
     while turns > 0:
         failed = 0
+        drawFigure(turns)
         for char in word:
             if char in guesses:
                 print (char, end = ""),
@@ -44,9 +45,32 @@ def play(word):
                 print ("You have", + turns, 'more guesses')
                 print()
     return turns
-                    
+
+def drawFigure(turns):
+    body_parts = ["  |   O", "  |   |", "  |  /|", "  |  /|\\", "  |  /", "  |  / \\"]
+    empty_scaffold = ["  +---+\n  |   |", "  |", "  |\n========="]
+    print(empty_scaffold[0])
+    match turns:
+        case 5:
+            print(body_parts[0], empty_scaffold[1], empty_scaffold[1],sep='\n')
+        case 4:
+            print(body_parts[0],body_parts[1], empty_scaffold[1],sep='\n')
+        case 3:
+            print(body_parts[0],body_parts[2], empty_scaffold[1],sep='\n')
+        case 2:
+            print(body_parts[0],body_parts[3], empty_scaffold[1],sep='\n')
+        case 1:
+            print(body_parts[0],body_parts[3],body_parts[4],sep='\n')
+        case 0:
+            print(body_parts[0],body_parts[3],body_parts[5],sep='\n')    
+        case _:
+            print(empty_scaffold[1],empty_scaffold[1],empty_scaffold[1],sep='\n')
+    print(empty_scaffold[2])
+
+
 def endgame(turns, word):
     if turns == 0:
+        drawFigure(turns)
         print ("say uncle if you would like to see the word")
         answer=input()
         if "uncle" in answer.lower():
